31df8774b7aa72c08e134d0af7490c956b4b0db0
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-02-28 13:44:49 +0100

    ej 2 y 3 comentados

--- Tema6StringsEjercicios2/src/ejercicios/Ejercicio2.java
+++ Tema6StringsEjercicios2/src/ejercicios/Ejercicio2.java
@@ -3,7 +3,66 @@ package ejercicios;
 public class Ejercicio2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		String codigo = "<!DOCTYPE html>\r\n"// guardara el codigo html dado
+				+ "<hotmail>\r\n" + "<body>\r\n" + "<h1></h1>\r\n" + "<p></p>\r\n" + "</body>\r\n" + "</hotmail>";
+
+		int posicionEncontrada;// guardara posicion devuelta por metodo indexOf
+
+		String cadenaTemporalMitad1 = "";// guardara el codigo hasta posicion de insercion de contenidoH1 o contenidoP
+
+		String cadenaTemporalMitad2 = "";// guardara el codigo desde posicion despues de insercion de contenidoH1 o
+											// contenidoP hasta final de codigo
+
+		String contenidoH1 = "Titulo";// guardara codigo a insertar en etiqueta h1
+
+		String contenidoP = "Parrafada aleatoria de turno";// guardara codigo a insertar en etiqueta p
+
+		// doy a posicionEncontrada valor igual al devuelto por el metodo indexOf al
+		// realizar busqueda de etiqueta de apertura de h1
+		posicionEncontrada = codigo.indexOf("<h1>");
+
+		// doy a cadenaTemporalMitad1 valor de codigo desde su principio hasta la
+		// posicion en la que encuentra a la etiqueta de apertura de h1 mas la longitud
+		// de la misma para incluirla tambien
+		cadenaTemporalMitad1 = codigo.substring(0, posicionEncontrada += "<h1>".length());
+
+		// reutilizo posicionEncontrada y le doy a posicionEncontrada valor igual al
+		// devuelto por metodo indexOf al realizar busqueda de etiqueta de cierre de h1
+		posicionEncontrada = codigo.indexOf("</h1>");
+
+		// doy a cadenaTemporalMitad2 valor de codigo desde posicion en la que encuentra
+		// a la etiqueta de cierre de h1 hasta final del codigo
+		cadenaTemporalMitad2 = codigo.substring(posicionEncontrada);
+
+		// machaco valor de codigo dandole el valor de cadenaTemporalMitad1
+		codigo = cadenaTemporalMitad1;
+
+		// añado valor de contenidoH1 a codigo
+		codigo += contenidoH1;
+
+		// añado valor de cadenaTemporalMitad2 a codigo
+		codigo += cadenaTemporalMitad2;
+
+		// realizo el mismo procedimiento sustituyendo las subcadenas usadas en las
+		// busquedas <h1> por <p> y </h1> por </p>
+
+		posicionEncontrada = codigo.indexOf("<p>");
+
+		cadenaTemporalMitad1 = codigo.substring(0, posicionEncontrada += "<p>".length());
+
+		posicionEncontrada = codigo.indexOf("</p>");
+
+		cadenaTemporalMitad2 = codigo.substring(posicionEncontrada);
+
+		codigo = cadenaTemporalMitad1;
+
+		codigo += contenidoP;
+
+		codigo += cadenaTemporalMitad2;
+
+		// muestro el nuevo codigo con contenido añadido a etiquetas por pantalla
+		System.out.println(codigo);
 
 	}
 
--- Tema6StringsEjercicios2/src/ejercicios/Ejercicio3.java
+++ Tema6StringsEjercicios2/src/ejercicios/Ejercicio3.java
@@ -1,10 +1,93 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String frase;// guardara la frase introducida por el usuario
+
+		String fraseDivididaPalabras[] = new String[0];// guardara las palabras de la frase , lo inicializo con longitud
+														// 0 y el metodo split le dara la longitud necesaria
+
+		String espacio = " ";// guardara el valor a usar en el metodo split, la inicializo con espacio en
+								// blanco para uso en metodo split
+
+		String palabraPosTemporal;// guardara la palabra que esta guardada en la posicion actual del array
+									// fraseDivididaPalabras
+
+		char letraEnMinuscula;// guardara primera letra de cada palabra
+
+		char letraEnMayuscula;// guardara primera letra de cada palabra ya convertida en mayuscula
+
+		// creo escaner y lo nombro dogma
+		Scanner dogma = new Scanner(System.in);
+
+		// solicito a usuario que introduzca frase con mensaje por pantalla
+		System.out.println("Introduzca su frase: ");
+
+		// doy a frase valor introducido por usuario con escaner
+		frase = dogma.nextLine();
+
+		// paso la frase a minusculas
+		frase = frase.toLowerCase();
+
+		// divido la frase en palabras separadas por espacios en blanco y las voy
+		// guardando en el array fraseDivididaPalabras, el metodo spli ira creando las
+		// posiciones necesarias
+		fraseDivididaPalabras = frase.split(espacio);
+
+		// bucle que recorrera el array fraseDivididaPalabras a excepcion de la primera
+		// palabra
+		// (cuya primera letra no necesita ser pasada a mayuscula)
+		// convirtiendo la primera letra de sus palabras en mayuscula
+		for (int i = 1; i < fraseDivididaPalabras.length; i++) {
+
+			// doy a palabraPosTemporal el valor de la cadena del array
+			// fraseDivididaPalabras en posicion i
+			palabraPosTemporal = fraseDivididaPalabras[i];
+
+			// doy a letraEnMinuscula el valor del primer caracter de la cadena
+			// palabraPosTemporal
+			letraEnMinuscula = palabraPosTemporal.charAt(0);
+
+			// doy a letraEnMayuscula valor de letraEnMinuscula pasado a mayuscula usando el
+			// metodo Character.toUpperCase que pasa un char de minuscula a mayuscula
+			letraEnMayuscula = Character.toUpperCase(letraEnMinuscula);
+
+			// reemplazo letraEnMinuscula por letraEnMayuscula
+			// (ambas en posicion charAt(0), o sea, primera letra)
+			// en cadena palabraPosTemporal
+			palabraPosTemporal = palabraPosTemporal.replace(letraEnMinuscula, letraEnMayuscula);
+
+			// sobreescribo la cadena contenida en array fraseDivididaPalabras posicion i
+			// con valor de cadena palabraPosTemporal, o lo que es lo mismo, cambio la
+			// palabra cuya inicial es minuscula por la palaba cuya inicial es mayuscula
+			fraseDivididaPalabras[i] = palabraPosTemporal;
+
+			// lo siguiente era un planteamiento sin el metodo Character.toUpperCase
+			// guardo primera letra de palabra en variable char letraEnMinuscula
+			// letraEnMinuscula = palabraPosTemporal.charAt(0);
+			// la añado a String letraEnMayuscula2
+			// letraEnMayuscula2 += letraEnMinuscula;
+			// la paso a mayuscula
+			// letraEnMayuscula2 = letraEnMayuscula2.toUpperCase();
+			// la paso a char otra vez
+			// letraEnMayuscula=letraEnMayuscula2.charAt(0);
+			// problema con no coger la primera letra de la siguientes palabras
+		}
+
+		// bucle que recorrera el array fraseDivididaPalabras dando a auxiliar el valor
+		// del elemento en la posicion actual mostrando todas las palabras que conforman
+		// la frase original
+		for (String auxiliar : fraseDivididaPalabras) {
+			// print por pantalla en misma linea de auxiliar
+			System.out.print(auxiliar);
+		}
+
+		// cierro escaner
+		dogma.close();
 	}
 
 }
