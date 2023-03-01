5833ef596742755cc3321ffd70a168f908c120b4
juanpesca19 <juanpesca19@DESKTOP-278S0F8>
2023-03-01 19:51:25 +0100

    ej 1 y ej 2 comentados

--- Tema6StringsEjercicios2/src/ejercicios/Ejercicio1.java
+++ Tema6StringsEjercicios2/src/ejercicios/Ejercicio1.java
@@ -6,46 +6,49 @@ public class Ejercicio1 {
 
 	public static void main(String[] args) {
 
-		String codigo = "";
+		String codigo;// guardara el codigo introducido por usuario
 
-		String codigoSinComentario = "";
+		int posInicio = 0;// guardara posicion devuelta por metodo indexOf aplicado a "/*",
+		// inicializo a 0 para uso en bucle
 
-		int posInicio = 0;
+		int posFinal = 0;// guardara posicion devuelta por metodo indexOf aplicado a "*/",
+		// inicializo a 0 para uso en bucle
 
-		int posFinal = 0;
-		
-		int nuevaPosInicio = 0;
+		String cadenaExtraida;// guardara cadena extraida de codigo con metodo substring
 
-		int nuevaPosFinal = 0;
-		
-		String cadenaExtraida="";
-
+		// creo escaner y lo nombro dogma
 		Scanner dogma = new Scanner(System.in);
 
+		// solicito al usuario su codigo
 		System.out.println("Introduzca su codigo: ");
 
+		// doy a codigo valor con escaner
 		codigo = dogma.nextLine();
 
-		posInicio = 0;
-
-		posFinal = codigo.indexOf("/*");
-		
-		codigoSinComentario += codigo.substring(posInicio, posFinal);
-		
+		// bucle que se ejecutara mientras posInicio y posFinal sean menores que la
+		// longitud de codigo, extrayendo una subcadena equivalente al comentario del
+		// codigo y sustituyendo esta por una vacia en la string codigo, borrando asi
+		// los comentarios
 		while (posInicio < codigo.length() && posFinal < codigo.length()) {
-			
-			
-			posInicio = codigo.indexOf("*/", posFinal);
 
-			posFinal = codigo.indexOf("/*", posFinal+2);
-			
-			if (posInicio < codigo.length() && posFinal < codigo.length()) {
-			codigoSinComentario += codigo.substring(posInicio+2, posFinal);
-			}
+			// doy a posInicio valor de posicion devuelta por metodo indexOf aplicado a "/*"
+			posInicio = codigo.indexOf("/*");
+
+			// doy a posFinal valor de posicion devuelta por metodo indexOf aplicado a "*/"
+			posFinal = codigo.indexOf("*/");
+
+			// doy a cadenaExtraida valor devuelto por metodo substring aplicado a codigo
+			cadenaExtraida = codigo.substring(posInicio, posFinal + 2);
+
+			// sustituyo la cadenaExtraida por una cadena vacia en la cadena codigo
+			codigo = codigo.replace(cadenaExtraida, "");
 		}
-		
-		System.out.println(codigoSinComentario);
 
+		// muestro el codigo sin comentarios con print por pantalla
+		System.out.println(codigo);
+
+		// cierro escaner
+		dogma.close();
 	}
 
 }
--- Tema6StringsEjercicios2/src/ejercicios/Ejercicio5.java
+++ Tema6StringsEjercicios2/src/ejercicios/Ejercicio5.java
@@ -1,10 +1,83 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		String palabra;// guardara la palabra introducida por usuario
+
+		int cantidad;// guardara cantidad en la que agrupar letras
+
+		int posInicio = 0;// guardara posicion desde la que empezar a extraer la subcadena
+
+		int posFinal;// guardara posicion hasta la que terminar de extraer la subcadena
+
+		int caracteresRelleno; // guardara cantidad de caracteres a añadir a cadena
+
+		String cadenaExtraida;// guardara cadena extraida de codigo con metodo substring
+
+		// creo escaner y lo nombro dogma
+		Scanner dogma = new Scanner(System.in);
+
+		// solicito al usuario su palabra
+		System.out.println("Introduzca su palabra: ");
+
+		// doy a palabra valor con escaner
+		palabra = dogma.nextLine();
+
+		// solicito al usuario su cantidad
+		System.out.println("Introduzca cantidad en que quiere agrupar las letras: ");
+
+		// doy a cantidad valor con escaner
+		cantidad = dogma.nextInt();
+
+		// doy a posFinal valor igual a cantidad
+		posFinal = cantidad;
+
+		// si resto de la cantidad de caracteres de la palabra dividida entre cantidad
+		// es diferente a 0
+		if (palabra.length() % cantidad != 0) {
+
+			// habra perdida de caracteres, para evitarlo añado caracteres de relleno que
+			// seran los que se pierdan en lugar de los ya existentes en la palabra, la
+			// cantidad necesaria de estos caracteres de relleno nunca sera mayor que la
+			// cantidad de caracteres en los que quiero dividir la palabra, por lo que puedo
+			// usar cantidad para dar valor a caracteresRelleno o cantidad-1
+			// si quiero usar la cantidad exacta de caracteres necesarios de relleno
+			caracteresRelleno = cantidad - 1;
+
+			// bucle para añadir el caracter de relleno tantas veces como sea necesario
+			// indicado por caracteresRelleno, uso espacio " " para no cambiar la palabra
+			for (int i = 1; i <= caracteresRelleno; i++) {
+				// añado el caracter de relleno a la palabra
+				palabra += " ";
+			}
+		}
+
+		// bucle que se ejecutara mientras posInicio y posFinal sean menor a longitud de
+		// palabra, extrayendo subcadenas de longitud indicada por cantidad y haciendo
+		// print de la misma en la misma iteracion
+		while (posInicio <= palabra.length() && posFinal <= palabra.length()) {
+
+			// doy a cadenaExtraida valor devuelto por metodo substring aplicado a palabra
+			cadenaExtraida = palabra.substring(posInicio, posFinal);
+
+			// hago print por pantalla de cadenaExtraida
+			System.out.println(cadenaExtraida);
+
+			// añado valor de cantidad a posInicio para actualizar la posicion desde la que
+			// extraere una subcadena en la siguiente iteracion
+			posInicio += cantidad;
+
+			// añado valor de cantidad a posFinal para actualizar la posicion hasta la que
+			// extraere una subcadena en la siguiente iteracion
+			posFinal += cantidad;
+		}
+
+		// cierro escaner
+		dogma.close();
 	}
 
 }
