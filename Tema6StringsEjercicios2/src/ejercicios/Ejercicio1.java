d98793fa9fb79e4c722d0264d8f1ef62507ef11d
juanpc19 <juan.perez@iesnervion.es>
2023-03-01 14:33:23 +0100

    ej 1 medio hecho planteamiento mal?

--- Tema6StringsEjercicios2/src/ejercicios/Ejercicio1.java
+++ Tema6StringsEjercicios2/src/ejercicios/Ejercicio1.java
@@ -1,10 +1,50 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
 
+		String codigo = "";
+
+		String codigoSinComentario = "";
+
+		int posInicio = 0;
+
+		int posFinal = 0;
 		
+		int nuevaPosInicio = 0;
+
+		int nuevaPosFinal = 0;
+		
+		String cadenaExtraida="";
+
+		Scanner dogma = new Scanner(System.in);
+
+		System.out.println("Introduzca su codigo: ");
+
+		codigo = dogma.nextLine();
+
+		posInicio = 0;
+
+		posFinal = codigo.indexOf("/*");
+		
+		codigoSinComentario += codigo.substring(posInicio, posFinal);
+		
+		while (posInicio < codigo.length() && posFinal < codigo.length()) {
+			
+			
+			posInicio = codigo.indexOf("*/", posFinal);
+
+			posFinal = codigo.indexOf("/*", posFinal+2);
+			
+			if (posInicio < codigo.length() && posFinal < codigo.length()) {
+			codigoSinComentario += codigo.substring(posInicio+2, posFinal);
+			}
+		}
+		
+		System.out.println(codigoSinComentario);
 
 	}
 
